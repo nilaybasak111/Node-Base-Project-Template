bb0e20df6ba47a55c53e161244e88080fd62b827
nilaybasak111 <100372393+nilaybasak111@users.noreply.github.com>
2025-05-02 08:16:26 +0530

    Added a Comment in index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const { InfoController } = require('../../controllers');
 
 const router = express.Router();
 
+// GET : /api/v1/info
 router.get('/info', InfoController.info);
 
 module.exports = router;
